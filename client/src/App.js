8f5f9edd6f244d7c3c43010dd7732a811fb739dc
Piotr Rzadkowolski <p.rzadkowolski@gmail.com>
2022-08-19 22:14:13 +0200

    build(client): add react-icons for devdependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
+import Navbar from "./components/Navbar/Navbar";
 import Loads from "./components/Loads/Loads";
 import Form from "./components/Form/Form";
 import { getLoads } from "./redux/loadSlice";
@@ -13,7 +14,9 @@ const App = () => {
 
   return (
     <div className="min-h-full">
-      <nav className="bg-gray-800">Navigation</nav>
+      <nav className="bg-gray-800 text-white">
+        <Navbar />
+      </nav>
 
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
