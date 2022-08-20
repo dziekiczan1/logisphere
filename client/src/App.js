a569bd7f82bb7420a4793b71a84e3729bb7cc465
Piotr Rzadkowolski <p.rzadkowolski@gmail.com>
2022-08-20 22:15:16 +0200

    build(client): add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Loads from "./components/Loads/Loads";
 import Form from "./components/Form/Form";
@@ -13,29 +14,33 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <div className="min-h-full">
-      <nav className="bg-gray-800 text-white">
-        <Navbar />
-      </nav>
+    <BrowserRouter>
+      <div className="min-h-full">
+        <nav className="bg-gray-800 text-white">
+          <Navbar />
+        </nav>
 
-      <header className="bg-white shadow">
-        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl tracking-tight font-bold text-gray-900">
-            Available loads
-          </h1>
-        </div>
-      </header>
-      <main>
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-2 border-gray-200 rounded-lg h-96">
-              <Loads />
-            </div>
-            <Form />
+        <header className="bg-white shadow">
+          <div className=" mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl tracking-tight font-bold text-gray-900">
+              Available loads
+            </h1>
           </div>
-        </div>
-      </main>
-    </div>
+        </header>
+        <main className="flex justify-start items-start">
+          <div className="w-5/6 mx-auto py-6 sm:px-6 lg:px-8">
+            <div className="px-4 py-6 sm:px-0">
+              <div className="border-4 border-2 border-gray-200 rounded-lg min-h-full">
+                <Routes>
+                  <Route path="/" element={<Loads />} />
+                  <Route path="/addloads" element={<Form />} />
+                </Routes>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 };
 
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { GiHolosphere } from "react-icons/gi";
 
 const Navbar = () => {
@@ -5,23 +6,25 @@ const Navbar = () => {
     <div className="flex flex-col justify-between p-2 md:flex-row md:p-4">
       <div className="flex items-center justify-center">
         <GiHolosphere className="text-6xl text-sky-500" />
-        <span className="text-4xl tracking-wide uppercase font-semibold">
-          Logisphere
-        </span>
+        <Link to="/">
+          <span className="text-4xl tracking-wide uppercase font-semibold">
+            Logisphere
+          </span>
+        </Link>
       </div>
       <div className="flex items-center justify-center">
-        <ul className="flex flex-row justify-center gap-2 mt-4 md:gap-4">
+        <ul className="flex flex-row justify-center gap-2 mt-4 md:gap-4 md:mt-0">
           <li class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium text-center">
-            <a href="#">Find loads</a>
+            <Link to="/">Find loads</Link>
           </li>
           <li class="text-gray-300 text-center hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-            <a href="#">My offers</a>
+            <Link to="/myoffers">My offers</Link>
           </li>
           <li class="text-gray-300 text-center hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-            <a href="#">My loads</a>
+            <Link to="/myloads">My loads</Link>
           </li>
           <li class="text-gray-300 text-center hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-            <a href="#">Post loads</a>
+            <Link to="/addloads">Post loads</Link>
           </li>
         </ul>
       </div>
