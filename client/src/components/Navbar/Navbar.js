06d50270b26fccc8c67dfca616bc7e7a71f1bfb8
Piotr Rzadkowolski <p.rzadkowolski@gmail.com>
2022-08-20 16:00:23 +0200

    feat(client/navbar): add navigation links

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,11 +2,23 @@ import { GiHolosphere } from "react-icons/gi";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-start items-center p-4">
-      <GiHolosphere className="text-6xl text-sky-500" />
-      <span className="text-4xl tracking-wide uppercase font-semibold">
-        Logisphere
-      </span>
+    <div className="flex flex-col justify-between p-4 md:flex-row">
+      <div className="flex items-center justify-center">
+        <GiHolosphere className="text-6xl text-sky-500" />
+        <span className="text-4xl tracking-wide uppercase font-semibold">
+          Logisphere
+        </span>
+      </div>
+      <div className="flex items-center justify-center">
+        <ul className="flex flex-col justify-center md:flex-row">
+          <li class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium text-center">
+            <a href="#">Find loads</a>
+          </li>
+          <li class="text-gray-300 text-center hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+            <a href="#">My offers</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
