a3d24f949a5643f9043165c8f1452e00fd4c0c5b
Piotr Rzadkowolski <p.rzadkowolski@gmail.com>
2022-08-22 19:36:27 +0200

    feat(client): add API for deleteLoad

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,3 +6,4 @@ export const fetchLoads = () => axios.get(url);
 export const createLoad = (newLoad) => axios.post(url, newLoad);
 export const updateLoad = (id, updatedLoad) =>
   axios.patch(`${url}/${id}`, updatedLoad);
+export const deleteLoad = (id) => axios.delete(`${url}/${id}`);
