7e998ce2d3a6a04bd492267f043adffec354e886
Piotr Rzadkowolski <p.rzadkowolski@gmail.com>
2022-08-23 19:25:40 +0200

    fix(client/auth): create Auth component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Form from "./components/Form/Form";
+import Auth from "./components/Auth/Auth";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/addloads" element={<Form />} />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </div>
     </BrowserRouter>
