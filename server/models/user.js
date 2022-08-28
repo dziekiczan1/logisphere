a12a543b7610f94ef34ecd4f82f00757c45cac66
dzekiczan1 <p.rzadkowolski@gmail.com>
2022-08-28 19:40:20 +0200

    feat(server): add admin property to userSchema

--- server/models/user.js
+++ server/models/user.js
@@ -4,6 +4,10 @@ const userSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  admin: {
+    type: Boolean,
+    default: false,
+  },
   id: { type: String },
 });
 
