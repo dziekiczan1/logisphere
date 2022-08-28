7a43bc3e130c55b05ca137012b4a8460e47dfc50
dzekiczan1 <p.rzadkowolski@gmail.com>
2022-08-28 20:21:20 +0200

    fix(client/load): add admin verification

--- client/src/components/Loads/Load/Load.js
+++ client/src/components/Loads/Load/Load.js
@@ -10,6 +10,9 @@ import { deleteLoad } from "../../../redux/loadSlice";
 
 const Load = ({ load, setCurrentId, handleShow }) => {
   const dispatch = useDispatch();
+  const user = JSON.parse(localStorage.getItem("profile"));
+  const isAdmin = user?.result?.admin === true;
+
   return (
     <div className="flex flex-col md:flex-row md:gap-4 md:my-2 text-md md:text-lg md:border md:border-l-4 border-transparent md:hover:shadow-md md:active:border md:active:border-l-4 md:active:border-sky-500 md:active:bg-slate-100">
       <div className="flex flex-col md:flex-row md:items-center w-full md:w-1/2 px-2 py-1 md:p-2 bg-slate-100">
@@ -55,23 +58,25 @@ const Load = ({ load, setCurrentId, handleShow }) => {
           <span>{load.price} USD</span>
           <span className="text-sky-500 text-sm">Book or Offer</span>
         </div>
-        <div className="flex flex-row justify-end md:flex-col md:justify-start md:items-center w-1/2 md:w-1/6">
-          <Button
-            onClick={() => {
-              handleShow();
-              setCurrentId(load._id);
-            }}
-            className="w-2/3"
-          >
-            <AiOutlineEdit className="text-lg" />
-          </Button>
-          <Button
-            onClick={() => dispatch(deleteLoad(load._id))}
-            className="w-2/3"
-          >
-            <AiOutlineDelete className="text-lg" />
-          </Button>
-        </div>
+        {isAdmin ? (
+          <div className="flex flex-row justify-end md:flex-col md:justify-start md:items-center w-1/2 md:w-1/6">
+            <Button
+              onClick={() => {
+                handleShow();
+                setCurrentId(load._id);
+              }}
+              className="w-2/3"
+            >
+              <AiOutlineEdit className="text-lg" />
+            </Button>
+            <Button
+              onClick={() => dispatch(deleteLoad(load._id))}
+              className="w-2/3"
+            >
+              <AiOutlineDelete className="text-lg" />
+            </Button>
+          </div>
+        ) : null}
       </div>
     </div>
   );
