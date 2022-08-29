908b1020fff352bd37a5b01cb9304e8a6377685e
dzekiczan1 <p.rzadkowolski@gmail.com>
2022-08-29 22:29:43 +0200

    feat(client/search): add Search component

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,7 +1,41 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const Search = () => {
-  return <div>Search</div>;
+  const [search, setSearch] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      searchPost();
+    }
+  };
+
+  const searchPost = () => {
+    if (search.trim()) {
+      // dispatch(getPostsBySearch({ search }));
+      navigate(`/loads/search?searchQuery=${search || "none"}`);
+    } else {
+      navigate("/");
+    }
+  };
+
+  return (
+    <>
+      <TextField
+        name="search"
+        label="From"
+        onKeyPress={handleKeyPress}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <Button onClick={searchPost}>Search</Button>
+    </>
+  );
 };
 
 export default Search;
