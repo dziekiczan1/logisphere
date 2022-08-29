8165614f42aa513b276033c1d7e8b118a9c9bf56
dzekiczan1 <p.rzadkowolski@gmail.com>
2022-08-29 17:57:13 +0200

    feat(client/loaddetails): add LD component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Form from "./components/Form/Form";
+import LoadDetails from "./components/LoadDetails/LoadDetails";
 import Auth from "./components/Auth/Auth";
 
 const App = () => {
@@ -15,6 +16,8 @@ const App = () => {
           <div className="w-full py-6 sm:px-6 lg:px-8">
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/loads" element={<Home />} />
+              <Route path="/loads/:id" element={<LoadDetails />} />
               <Route path="/addloads" element={<Form />} />
               <Route path="/auth" element={<Auth />} />
             </Routes>
---
+++ client/src/components/LoadDetails/LoadDetails.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoadDetails = () => {
+  return <div>LoadDetails</div>;
+};
+
+export default LoadDetails;
